9b9a686432469c54a457c914781f7316b4c69e75
Paul Creaser <drpaulanthonycreaser@gmail.com>
2021-04-06 16:15:35 +0900

    [add] Checkpoints

--- clock_train.py
+++ clock_train.py
@@ -13,7 +13,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-img_width, img_height = 144, 144
+img_width, img_height = 120, 120
 
 def test_preprocess(image):
     image = tensorflow.image.rgb_to_grayscale(image)
@@ -284,6 +284,44 @@ def model_build_simple_2( img_height, img_width, depth=3 ):
 
     return  Model( [ input_img ], outputs = output )
 
+
+def model_build_simple_4col( img_height, img_width, depth=3 ):
+    # Model build
+    input_shape = ( img_height, img_width, depth )
+    input_img = Input( shape=input_shape )
+    # Feature Extraction For Both Hands
+    feature_1 = Conv2D(64, (3,3),  activation='relu')( input_img ) # padding='same',
+    feature_1_scaled = MaxPooling2D( (2,2))( feature_1  )
+    #scaled_10  = BatchNormalization() (scaled_10a)
+
+    feature_2 = Conv2D(64, (3,3), padding='same', activation='relu')( feature_1_scaled ) # batch_10 )
+    feature_2_scaled = MaxPooling2D( (2,2))( feature_2 )
+
+    feature_3 = Conv2D(72, (3,3), activation='relu')( feature_2_scaled ) # , padding='same'
+    feature_2_scaled = MaxPooling2D( (2,2))( feature_3  )
+
+    # Minute hand features
+    flat_1  = Flatten()( feature_2_scaled )
+
+    dense1  = Dense(48, activation='relu')(flat_1)
+
+    mn21    = Dense(12, activation='relu')(dense1)
+    mn22    = Dense(12, activation='relu')(dense1)
+
+    hr21    = Dense(12, activation='relu')(dense1)
+    hr22    = Dense(12, activation='relu')(dense1)
+
+    # Rounded
+    mn31    = Dense(1, activation='tanh')(mn21)
+    mn32    = Dense(1, activation='tanh')(mn22)
+
+    hr31    = Dense(1, activation='tanh')(hr21)
+    hr32    = Dense(1, activation='tanh')(hr22)
+
+    output = concatenate( [ hr31, hr32, mn31, mn32 ] )
+    return  Model( [ input_img ], outputs = output )
+
+
 def model_build( img_height, img_width, depth=3 ):
     # Model build
     input_shape = ( img_height, img_width, depth )
@@ -338,7 +376,55 @@ def model_build( img_height, img_width, depth=3 ):
     output = concatenate( [ hr41, hr42, mn41, mn42, hrnd41, hrnd42 ] )
     return  Model( [ input_img ], outputs = output )
 
-def clock_train( csv, epochs=200, batch_size=2, saved_model=None ):
+
+
+def model_build_col_4( img_height, img_width, depth=3 ):
+    # Model build
+    input_shape = ( img_height, img_width, depth )
+    input_img = Input( shape=input_shape )
+    # Feature Extraction For Both Hands
+    feature_1 = Conv2D(64, (3,3),  activation='relu')( input_img ) # padding='same',
+    feature_1_scaled = MaxPooling2D( (2,2))( feature_1  )
+    #scaled_10  = BatchNormalization() (scaled_10a)
+
+    feature_2 = Conv2D(64, (3,3), padding='same', activation='relu')( feature_1_scaled ) # batch_10 )
+    feature_2_scaled = MaxPooling2D( (2,2))( feature_2 )
+
+    all_feature = Conv2D(70, (3,3), activation='relu')( feature_2_scaled ) # , padding='same'
+
+    all_features = MaxPooling2D( (2,2) )( all_feature  )
+    # Minute hand features
+    flat  = Flatten()( all_features )
+
+    all11   = Dense(60, activation='relu')(flat)
+    all12   = Dense(60, activation='relu')(flat)
+
+    #
+    mn21    = Dense(18, activation='relu')(all11)
+    mn22    = Dense(18, activation='relu')(all12)
+
+    hr21    = Dense(18, activation='relu')(all11)
+    hr22    = Dense(18, activation='relu')(all12)
+
+    mn31    = Dense(4, activation='relu')(mn21)
+    mn32    = Dense(4, activation='relu')(mn22)
+
+    hr31    = Dense(4, activation='relu')(hr21)
+    hr32    = Dense(4, activation='relu')(hr22)
+
+    # Last
+    mn41    = Dense(1, activation='tanh')(mn31)
+    mn42    = Dense(1, activation='tanh')(mn32)
+
+    hr41    = Dense(1, activation='tanh')(hr31)
+    hr42    = Dense(1, activation='tanh')(hr32)
+
+    output = concatenate( [ hr41, hr42, mn41, mn42 ] )
+    return  Model( [ input_img ], outputs = output )
+
+
+
+def clock_train( csv, epochs=200, batch_size=2, saved_model=None, checkpoint_dir=None ):
     df=pd.read_csv(csv, sep=',')
     print( df.head())
     nb_train_samples      = len(df.index)
@@ -357,8 +443,10 @@ def clock_train( csv, epochs=200, batch_size=2, saved_model=None ):
         print(column_list, type(column_list) )
 
     # model = model_build_couple( img_height, img_width )
-    #model = model_build_old( img_height, img_width )
-    model = model_build_simple_2( img_height, img_width )
+    # model = model_build_old( img_height, img_width )
+    # model = model_build_simple_2( img_height, img_width )
+    # model = model_build_simple_4col( img_height, img_width )
+    model = model_build_col_4( img_height, img_width )
 
     model.compile(optimizer='adam', loss='mse', metrics =['accuracy'])
     print( model.summary() )
@@ -398,11 +486,20 @@ def clock_train( csv, epochs=200, batch_size=2, saved_model=None ):
         batch_size =  batch_size,
         class_mode ='raw' )
 
+    model_checkpoint_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_dir,
+        save_weights_only=False,
+        monitor='val_accuracy',
+        mode='max',
+        save_best_only=True
+    )
+
     # Fit Model
     model.fit(train_generator,
         steps_per_epoch = nb_train_samples // batch_size,
         epochs = epochs, validation_data = validation_generator,
-        validation_steps = nb_validation_samples // batch_size)
+        validation_steps = nb_validation_samples // batch_size,
+        callbacks=[model_checkpoint_callback] )
 
     # Save model
     if saved_model is not None:
@@ -413,32 +510,35 @@ def main( argv ):
     """
     :param argv: Command line arguments
     """
-    csv = None # CSV File
-    model_file = None
+    csv            = None # CSV File
+    model_file     = None
+    checkpoint_dir = None
     try:
-        opts, args = getopt.getopt(argv,"hc:m:",["csv", "model"])
+        opts, args = getopt.getopt(argv,"hc:m:k:",["csv", "model", "checkpoint" ])
     except getopt.GetoptError:
-        print('python clockReaderTrain.py -c <csv> -m <model>')
+        print('python clockReaderTrain.py -c <csv> -m <model> -k <checkpoint>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('python clockReaderTrain.py -c <csv> -m <model>')
+            print('python clockReaderTrain.py -c <csv> -m <model> -k <checkpoint>')
             sys.exit()
         elif opt in ("-c", "--csv"):
             csv = arg
         elif opt in ("-m", "--model"):
             model_file = arg
+        elif opt in ("-k", "--checkpoint"):
+            checkpoint_dir = arg
 
     if csv is None or model_file is None:
-        print('python clockReaderTrain.py -c <csv> -m <model>')
+        print('python clockReaderTrain.py -c <csv> -m <model> -k <checkpoint>')
         exit(2)
 
     print(" Csv file ", csv, " Model ", model_file  )
-    clock_train( csv, epochs=120, batch_size=3, saved_model=model_file )
+    clock_train( csv, epochs=120, batch_size=6, saved_model=model_file, checkpoint_dir=checkpoint_dir )
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print('python clockReaderTrain.py -c <csv> -m <model>')
+    if len(sys.argv) != 7:
+        print('python clockReaderTrain.py -c <csv> -m <model> -k <checkpoint>')
         sys.exit(2)
     main(sys.argv[1:])
